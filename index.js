f37f49d59bfffb5fb7561cd9e658334fcc97cd53
ashok singh <bsbldigital@gmail.com>
2024-11-01 11:08:52 +0530

    objectData

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ import "dotenv/config";
 const app = express();
 const port = 3000;
 
+const objData = {
+  userId: 1,
+  id: 1,
+  title:
+    "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+  body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+};
 app.get("/", (req, res) => {
   res.send("Hello nodejs world");
 });
@@ -14,6 +21,10 @@ app.get("/login", (req, res) => {
 });
 app.get("/youtube", (req, res) => {
   res.send("Now watch the youtube!");
+});
+
+app.get("/objectdata", (req, res) => {
+  res.json(objData);
 });
 
 app.listen(port, () => {
